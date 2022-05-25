40854310bfd1303eeecb7481d53a269f5d010e9d
ShriyaChandran <62100109+ShriyaChandran@users.noreply.github.com>
2022-05-25 21:14:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -9,37 +9,46 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('data.csv')
 print(df.head())
 
-variance = df["variance"]
-type = df["type"]
-variance_train, variance_test, type_train, type_test = train_test_split(variance, type, test_size = 0.25, random_state = 0)
+y = df['class']
+X = df[['variance','skewness','curtosis','entropy']]
 
-X = np.reshape(variance_train.ravel(), (len(variance_train), 1))
-Y = np.reshape(type_train.ravel(), (len(type_train), 1))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(X, Y)
+#X = np.reshape(variance_train.ravel(), (len(variance_train), 1))
+#Y = np.reshape(type_train.ravel(), (len(type_train), 1))
 
-X_test = np.reshape(variance_test.ravel(), (len(variance_test), 1))
-Y_test = np.reshape(type_test.ravel(), (len(type_test), 1))
+#classifier = LogisticRegression(random_state = 0)
+#classifier.fit(X, Y)
 
-type_prediction = classifier.predict(X_test)
+#X_test = np.reshape(variance_test.ravel(), (len(variance_test), 1))
+#Y_test = np.reshape(type_test.ravel(), (len(type_test), 1))
+
+#type_prediction = classifier.predict(X_test)
+
+
+LR = LogisticRegression()
+LR.fit(X_train,y_train) #fitting the model 
+
+y_prediction = LR.predict(X_test)
+
+
 
 predicted_values = []
-for i in type_prediction:
-    if i == 0:
-        predicted_values.append("Authorized")            
-    else:
-        predicted_values.append("Forged")
+for i in y_prediction:
+  if i == 0:
+    predicted_values.append("Authorized")
+  else:
+    predicted_values.append("Forged")
 
 actual_values = []
-for i in Y_test.ravel():
-    if i == 0:
-        actual_values.append("Authorized")
-    else:
-        actual_values.append("Forged")
+for i in y_test:
+  if i == 0:
+    actual_values.append("Authorized")
+  else:
+    actual_values.append("Forged")
 
 
-labels = ["FOrged", "Authorized"]
+labels = ["Forged", "Authorized"]
 
 cm = confusion_matrix(actual_values, predicted_values)
 
@@ -50,4 +59,4 @@ ax.set_xlabel('Predicted')
 ax.set_ylabel('Actual') 
 ax.set_title('Confusion Matrix')
 ax.xaxis.set_ticklabels(labels); ax.yaxis.set_ticklabels(labels)
-plt.show()
+#fig.show()
